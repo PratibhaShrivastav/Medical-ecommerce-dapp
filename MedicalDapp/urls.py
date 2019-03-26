76985d813de72f2d995fa8a5a47abd241ce3499d
PratibhaShrivastav <pratibha.shrivastav99@gmail.com>
2019-03-26 17:52:15 +0530

    Login signup pages added

--- MedicalDapp/urls.py
+++ MedicalDapp/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Home.as_view(),name='home'),
     path('accounts/',include('accounts.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
