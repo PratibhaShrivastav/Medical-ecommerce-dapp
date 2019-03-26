15e0fae729b19d6434f21e463d49d5ccbf024415
PratibhaShrivastav <pratibha.shrivastav99@gmail.com>
2019-03-26 22:07:24 +0530

    cart added text extraction done

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from django.contrib.auth import views
-from .views import Signup
+from accounts import views
+from django.contrib.auth.views import LoginView,LogoutView
+from .views import Signup,Addtocart,Removefromcart
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(template_name='login.html'),name='login'),
-    path('logout/', views.LogoutView.as_view(),name='logout',kwargs={'next_page':'/'}),
+    path('login/', LoginView.as_view(template_name='login.html'),name='login'),
+    path('logout/', LogoutView.as_view(),name='logout',kwargs={'next_page':'/'}),
     path('signup/',Signup.as_view(),name='signup'),
+    path('<int:pk>/add-to-cart/',views.Addtocart,name="addtocart"),
+    path('<int:pk>/remove-from-cart/',views.Removefromcart,name="removefromcart"),
 ]
